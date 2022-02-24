ad5291bc37dd93706b0760068962101fcd00601c
Ian Chapman <e-n_chapman123@hotmail.com>
2022-02-24 17:27:54 -0500

    Added music manager script with List and piano notes

---
+++ Assets/[Scripts]/MusicManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+
+    [SerializeField]
+    List<AudioClip> pianoNotes;
+
+    [SerializeField]
+    private AudioSource soundSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
