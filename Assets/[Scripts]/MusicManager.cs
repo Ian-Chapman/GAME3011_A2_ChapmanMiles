cd3fbc6a5db64cf1b8927c0e1a8d75c61dbf9bb1
Ian Chapman <e-n_chapman123@hotmail.com>
2022-02-24 19:40:59 -0500

    Animations are playing with animation events playing music notes

--- Assets/[Scripts]/MusicManager.cs
+++ Assets/[Scripts]/MusicManager.cs
@@ -14,12 +14,94 @@ public class MusicManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+
+    //------------------------------------------------------------- Music notes played - attached to Piano Key animation events -----------------------------------------------
+    public void notePressedC()
+    {
+        soundSource.clip = pianoNotes[0];
+        soundSource.Play();
+    }
+
+    public void notePressedCSharp()
+    {
+        soundSource.clip = pianoNotes[1];
+        soundSource.Play();
+    }
+
+    public void notePressedD()
+    {
+        soundSource.clip = pianoNotes[2];
+        soundSource.Play();
+    }
+
+    public void notePressedDSharp()
+    {
+        soundSource.clip = pianoNotes[3];
+        soundSource.Play();
+    }
+
+    public void notePressedE()
+    {
+        soundSource.clip = pianoNotes[4];
+        soundSource.Play();
+    }
+
+    public void notePressedF()
+    {
+        soundSource.clip = pianoNotes[5];
+        soundSource.Play();
+    }
+
+    public void notePressedFSharp()
+    {
+        soundSource.clip = pianoNotes[6];
+        soundSource.Play();
+    }
+
+    public void notePressedG()
+    {
+        soundSource.clip = pianoNotes[7];
+        soundSource.Play();
+    }
+
+    public void notePressedGSharp()
+    {
+        soundSource.clip = pianoNotes[8];
+        soundSource.Play();
+    }
+
+    public void notePressedA()
+    {
+        soundSource.clip = pianoNotes[9];
+        soundSource.Play();
+    }
+
+    public void notePressedASharp()
+    {
+        soundSource.clip = pianoNotes[10];
+        soundSource.Play();
+    }
+
+    public void notePressedB()
+    {
+        soundSource.clip = pianoNotes[11];
+        soundSource.Play();
+    }
+
+    public void notePressedHighC()
+    {
+        soundSource.clip = pianoNotes[12];
+        soundSource.Play();
+    }
+
+
 }
